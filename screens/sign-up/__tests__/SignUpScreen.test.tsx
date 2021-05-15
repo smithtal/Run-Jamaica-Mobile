89ecdc5396e7c8306af3b775c1c050e288dae326
Travis Smith <travis.a.smith.ja@gmail.com>
2021-05-15 17:56:36 -0500

    Adds test for 409 error message

---
+++ screens/sign-up/__tests__/SignUpScreen.test.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  waitForElementToBeRemoved,
+  waitFor,
+  RenderAPI,
+} from '@testing-library/react-native';
+import * as authServices from '../../../services/auth';
+import SignUpScreen from '../SignUpScreen';
+import {AuthContext} from '../../../components/auth/auth.context';
+
+describe('SignUpScreen', () => {
+  let authContextSetAccessTokenSpy: jest.Mock<string, any>;
+  let authContextSetRefreshTokenSpy: jest.Mock<string, any>;
+
+  const renderForm = () => {
+    return render(
+      <AuthContext.Provider
+        value={{
+          setAccessToken: authContextSetAccessTokenSpy,
+          setRefreshToken: authContextSetRefreshTokenSpy,
+        }}>
+        <SignUpScreen />
+      </AuthContext.Provider>,
+    );
+  };
+
+  const testName = 'Test User';
+  const testEmailAddress = 'test@example.net';
+  const testPassword = 'Password123';
+
+  const completeForm = (
+    wrapper: RenderAPI,
+    {
+      name = testName,
+      emailAddress = testEmailAddress,
+      password = testPassword,
+      confirmPassword = testPassword,
+    } = {},
+  ) => {
+    const {getByText, getByPlaceholderText} = wrapper;
+
+    fireEvent(getByPlaceholderText('Name'), 'onChangeText', name);
+
+    fireEvent(getByPlaceholderText('Email'), 'onChangeText', emailAddress);
+
+    fireEvent(getByPlaceholderText('Password'), 'onChangeText', password);
+
+    fireEvent(
+      getByPlaceholderText('Confirm Password'),
+      'onChangeText',
+      confirmPassword,
+    );
+
+    fireEvent(getByText('SIGN UP'), 'onPress');
+  };
+
+  beforeEach(() => {
+    authContextSetAccessTokenSpy = jest.fn();
+    authContextSetRefreshTokenSpy = jest.fn();
+  });
+
+  it('renders without error', () => {
+    renderForm();
+  });
+
+  it('when the form is completed and submitted the signup service is called', async () => {
+    const signUpSpy = jest.spyOn(authServices, 'signup').mockImplementationOnce(
+      (
+        _signupRequestBody: authServices.SignupRequestBody,
+      ): Promise<authServices.AuthResponse> => {
+        return Promise.resolve({
+          accessToken: 'accessToken',
+          refreshToken: 'refreshToken',
+        });
+      },
+    );
+
+    const wrapper = renderForm();
+
+    completeForm(wrapper);
+
+    await waitFor(() => {
+      expect(signUpSpy).toHaveBeenCalledWith({
+        name: 'Test User',
+        emailAddress: 'test@example.net',
+        password: 'Password123',
+        confirmPassword: 'Password123',
+      });
+      expect(authContextSetAccessTokenSpy).toHaveBeenCalledWith('accessToken');
+      expect(authContextSetRefreshTokenSpy).toHaveBeenCalledWith(
+        'refreshToken',
+      );
+    });
+  });
+
+  it('when the signup service returns a 409 status an error message says the emailAddress is taken', async () => {
+    const signUpSpy = jest
+      .spyOn(authServices, 'signup')
+      .mockRejectedValue({response: {status: 409}});
+
+    const wrapper = renderForm();
+
+    completeForm(wrapper);
+
+    const {getByText} = wrapper;
+
+    await waitFor(() => {
+      expect(getByText('This email address is already registered.'));
+    });
+  });
+});
