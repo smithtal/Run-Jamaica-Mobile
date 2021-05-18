90051a181e23db192b2dbbd0bf5b4d78ec28b92e
Travis Smith <travis.a.smith.ja@gmail.com>
2021-05-17 20:32:23 -0500

    Configures and install Detox

---
+++ e2e/splash.e2e.js
@@ -0,0 +1,13 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp({newInstance: true});
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('shows the splash screen upon first load', async () => {
+    await expect(element(by.id('splash-screen'))).toBeVisible();
+  });
+});
