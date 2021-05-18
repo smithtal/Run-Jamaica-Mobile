d3a7632e34f4cee98ce2114e6a511264bcbe5b6e
Travis Smith <travis.a.smith.ja@gmail.com>
2021-05-17 20:40:51 -0500

    Adds test for navigation

--- e2e/splash.e2e.js
+++ e2e/splash.e2e.js
@@ -10,4 +10,9 @@ describe('Splash Screen', () => {
   it('shows the splash screen upon first load', async () => {
     await expect(element(by.id('splash-screen'))).toBeVisible();
   });
+
+  it('shows the sign up screen when the sign up button is clicked', async () => {
+    await element(by.text('SIGN UP')).tap();
+    await expect(element(by.text('Create Account,'))).toBeVisible();
+  });
 });
